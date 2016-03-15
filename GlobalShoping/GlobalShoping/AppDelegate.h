1974266156f0711f8611d51cd97df75025d45119
WangXueJuan <1216747227@qq.com>
2016-03-15 21:19:29 +0800

    01项目框架

---
+++ GlobalShoping/GlobalShoping/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  GlobalShoping
+//
+//  Created by scjy on 16/3/15.
+//  Copyright © 2016年 王雪娟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic, strong) UITabBarController *tablebarVC;
+
+
+@end
+
